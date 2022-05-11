cfcc626fc34a2da192a3dc040160f4886248d754
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-05-11 11:10:36 +0530

    Can evaluate the result of one guess

--- evaluateGuess.js
+++ evaluateGuess.js
@@ -6,3 +6,20 @@ const isWordValid = (guessedWord) => {
   const validWords = allWords.filter((word) => word.length === FIVE);
   return validWords.includes(guessedWord);
 };
+
+const positionStatus = (actual, guessed, index) => {
+  if (actual[index] === guessed[index]) {
+    return 'correct';
+  }
+  return actual.includes(guessed[index]) ? 'present' : 'absent';
+};
+
+const evaluateGuess = (actual, guessed) => {
+  const guessResult = [];
+  for (let index = 0; index < actual.length; index++) {
+    const character = guessed[index];
+    const position = positionStatus(actual, guessed, index);
+    guessResult.push({ character, position });
+  }
+  return guessResult;
+};
